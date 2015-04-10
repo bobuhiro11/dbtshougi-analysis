01b461a447d5f22705e74b0defdea2e00bbce9a3
bobuhiro11 <nob@bobuhiro11.net>
2015-04-11 00:50:28 +0900

    cd $PBS_O_WORKDIR を使うことで qsub 実行場所へ移動して実行

--- create_all_board.sh
+++ create_all_board.sh
@@ -2,10 +2,8 @@
 #PBS -V
 #PBS -l nodes=1:ppn=1
 #PBS -l walltime=6:00:00
-#PBS -o /home/n-miki/dobutushogi-create-all-board.log
 
-script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
-cd ${script_dir}
+cd $PBS_O_WORKDIR # qsubを実行したディレクトリへ移動
 
 g++ -DMAX_BOARD_NUM=300000000 create_all_board.cpp
 ./a.out
