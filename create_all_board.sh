6a0e6e874db70020a7a72f10e29d5333270cb810
bobuhiro11 <nob@bobuhiro11.net>
2015-04-11 00:42:35 +0900

    .gitignore を追加
    - create_all_board.sh内のsourceを削除

--- create_all_board.sh
+++ create_all_board.sh
@@ -4,7 +4,6 @@
 #PBS -l walltime=6:00:00
 #PBS -o /home/n-miki/dobutushogi-create-all-board.log
 
-source /home/n-miki/.bashrc
 script_dir=$(cd $(dirname ${BASH_SOURCE:-$0}); pwd)
 cd ${script_dir}
 
