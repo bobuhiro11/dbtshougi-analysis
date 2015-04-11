96b6b8940c0b8ed2290a1af013962b271a3aa5bf
bobuhiro11 <nob@bobuhiro11.net>
2015-04-12 01:33:41 +0900

    all_state_removeend.dat を正しく出力できた

---
+++ remove_end_board.cpp
@@ -0,0 +1,48 @@
+/*
+ * 末端局面を削除する
+ */
+#include <sys/types.h>
+#include <sys/stat.h>
+#include <fcntl.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <stdio.h>
+#include <stdint.h>
+#include <limits.h>
+#include <time.h>
+#include <vector>
+#include <iostream>
+#include <fstream>
+#include <deque>
+#include <set>
+#include <map>
+#include <algorithm>
+#include "common.h"
+
+int main(int argc, char *argv[])
+{
+    // 末端局面を含む all-state.dat を読込
+    unsigned long file_size     = get_file_size("all-state.dat");
+    unsigned long all_state_num = file_size / sizeof(board);
+    vector<board> all_state;
+    vector<board> all_state_removeend;
+
+    all_state.resize(all_state_num);
+    ifstream ifs("all-state.dat");
+    ifs.read((char*)&all_state[0], all_state.size() * sizeof(board));
+
+    // 削除処理
+    for (int i=0; i<all_state_num; i++) {
+        board b = all_state[i];
+        if (!is_win_state(b) && !is_lose_state(b)) {
+            all_state_removeend.push_back(b);
+        }
+    }
+
+    // 末端局面を含まない all-state-removeend.dat に書込
+    fprintf(stderr, "num of all-state-removeend = %ld\n", all_state_removeend.size());
+    ofstream ofs("all-state_removeend.dat");
+    ofs.write((char *)&all_state_removeend[0],all_state_removeend.size()*sizeof(board));
+
+    return 0;
+}
---
+++ remove_end_board.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+#PBS -V
+#PBS -l nodes=1:ppn=1
+#PBS -l walltime=6:00:00
+
+#
+# ジュブスケジューラ TORQUE を使うためのスクリプト
+#
+
+cd $PBS_O_WORKDIR # qsubを実行したディレクトリへ移動
+
+g++ remove_end_board.cpp common.cpp && ./a.out
